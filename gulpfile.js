a9feef3fe4737aa935c658d2b4f5607ecac67203
Aaron Bentley <aaron@AB-MacBook-Pro.local>
2017-10-20 22:52:02 +0100

    Updated gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,109 @@
-const browserify = require("browserify")
-const gulp = require("gulp")
-const haml = require("gulp-haml")
-const sass = require("gulp-sass")
-const typescript = require("gulp-typescript")
-const uglify = require("gulp-uglify")
-const imagemin = require("gulp-imagemin")
-const webserver = require("gulp-webserver")
-const source = require("vinyl-source-stream")
-const buffer = require("vinyl-buffer")
-const sourcemaps = require("gulp-sourcemaps")
-const gutil = require("gulp-util")
+const browserify = require('browserify')
+const gulp = require('gulp')
+const haml = require('gulp-haml')
+const sass = require('gulp-sass')
+const typescript = require('gulp-typescript')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const webserver = require('gulp-webserver')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const sourcemaps = require('gulp-sourcemaps')
+const gutil = require('gulp-util')
 
 // Default task
-gulp.task("default", ["message", "copy-node-packages", "haml", "sass", "javascript", "imageMin", "watch", "webserver"])
+gulp.task('default', ['message', 'copy-node-packages', 'haml', 'sass', 'javascript', 'imageMin', 'watch', 'webserver'])
 
 // Copy modules from /node_modules/
-gulp.task("copy-node-packages", () => {
+gulp.task('copy-node-packages', () => {
     let assets = [
-        "./node_modules/jquery/**",
-        "./node_modules/popper.js/**",
-        "./node_modules/bootstrap/**",
-        "./node_modules/jquery.easing/**",
-        "./node_modules/scrollreveal/**",
-        "./node_modules/magnific-popup/**",
-        "./node_modules/font-awesome/**",
-        "./node_modules/spectragram/**"
+        './node_modules/jquery/**',
+        './node_modules/popper.js/**',
+        './node_modules/bootstrap/**',
+        './node_modules/jquery.easing/**',
+        './node_modules/scrollreveal/**',
+        './node_modules/magnific-popup/**',
+        '!./node_modules/magnific-popup/website/**',
+        './node_modules/font-awesome/**',
+        './node_modules/spectragram/**'
     ]
 
     assets.forEach(asset => {
-        gulp.src(asset, { base: "./node_modules/" }).pipe(gulp.dest("./dist/lib/"))
+        gulp.src(asset, { base: './node_modules/' }).pipe(gulp.dest('./dist/lib/'))
     })
 })
 
 // Watch source files for changes
-gulp.task("watch", () => {
-    gulp.watch("./src/haml/*.haml", ["haml"])
-    gulp.watch("./src/scss/*.scss", ["sass"])
-    gulp.watch("./src/js/*.js", ["javascript"])
-    gulp.watch("./src/img/*", ["imageMin"])
+gulp.task('watch', () => {
+    gulp.watch('./src/haml/*.haml', ['haml'])
+    gulp.watch('./src/scss/*.scss', ['sass'])
+    gulp.watch('./src/js/*.js', ['javascript'])
+    gulp.watch('./src/img/*', ['imageMin'])
 })
 
 // Log message
-gulp.task("message", () => {
-    console.log("Gulp is running...")
+gulp.task('message', () => {
+    console.log('Gulp is running...')
 })
 
 // Compile .haml
-gulp.task("haml", () => {
+gulp.task('haml', () => {
     gulp
-        .src("./src/haml/*.haml")
+        .src('./src/haml/*.haml')
         // .pipe(haml().on('error', haml.logError))
         .pipe(haml())
-        .pipe(gulp.dest("./dist/"))
+        .pipe(gulp.dest('./dist/'))
 })
 
 // Compile .scss
-gulp.task("sass", () => {
+gulp.task('sass', () => {
     gulp
-        .src("./src/scss/*.scss")
+        .src('./src/scss/*.scss')
         .pipe(
             sass({
                 includePaths: [
-                    "node_modules/bootstrap/scss"
+                    'node_modules/bootstrap/scss'
                     // 'node_modules/font-awesome/scss'
                 ]
-            }).on("error", sass.logError)
+            }).on('error', sass.logError)
         )
-        .pipe(gulp.dest("./dist/css"))
+        .pipe(gulp.dest('./dist/css'))
 })
 
 // Compile .js
-gulp.task("javascript", () => {
+gulp.task('javascript', () => {
     // set up the browserify instance on a task basis
     var b = browserify({
-        entries: "./src/js/app.js",
-        transform: ["babelify"],
+        entries: './src/js/app.js',
+        transform: ['babelify'],
         debug: true
     })
 
     return (
         b
             .bundle()
-            .pipe(source("app.js"))
+            .pipe(source('app.js'))
             .pipe(buffer())
             .pipe(sourcemaps.init({ loadMaps: true }))
             // Add transformation tasks to the pipeline here.
             .pipe(uglify())
-            .on("error", gutil.log)
-            .pipe(sourcemaps.write("./"))
-            .pipe(gulp.dest("./dist/js/"))
+            .on('error', gutil.log)
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist/js/'))
     )
 })
 
 // Optimize images
-gulp.task("imageMin", () =>
+gulp.task('imageMin', () =>
     gulp
-        .src("src/img/**/*")
+        .src('src/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img/"))
+        .pipe(gulp.dest('dist/img/'))
 )
 
 // Launch webserver
-gulp.task("webserver", () => {
-    gulp.src("./dist").pipe(
+gulp.task('webserver', () => {
+    gulp.src('./dist').pipe(
         webserver({
             livereload: true,
             directoryListing: false,
